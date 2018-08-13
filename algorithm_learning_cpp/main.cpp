c828f443df99d6bf408269599db94fcec9f736d3
Unknown <610733719@qq.com>
2018-08-13 22:45:01 +0800

    fix

--- algorithm_learning_cpp/main.cpp
+++ algorithm_learning_cpp/main.cpp
@@ -7,19 +7,18 @@ using namespace std;
 
 int main() {
 
-//    dynamicProgramming dynamicProgramming1;
+    dynamicProgramming dynamicProgramming1;
 //    vector<int>a = {2,1,3,1,1,2};
 //    int b = dynamicProgramming1.houseRobber(a);
 //    vector<int>a = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
 //    int b = dynamicProgramming1.minCostClimbingStairs(a);
-//    int memo[11] = {};
-//    int b = dynamicProgramming1.cutRod(memo, 4);
-//    cout << b;
+    int memo[11] = {};
+    int b = dynamicProgramming1.cutRod(memo, 4);
+    cout << b;
 
-    binaryTree binaryTree1;
-    TreeNode *root = NULL;
-//    root->val = 4;
-    TreeNode *root1 = binaryTree1.invertTree(root);
-    cout << root1;
+//    binaryTree binaryTree1;
+//    TreeNode *root = NULL;
+//    TreeNode *root1 = binaryTree1.invertTree(root);
+//    cout << root1;
     return 0;
 }
